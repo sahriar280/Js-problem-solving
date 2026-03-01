d5a95138b4e324f09fcd541f88cb93ef4da3f08f
Md.Sahriar Alam  Siyam <hackinghobby06@gmail.com>
2026-03-01 11:55:18 +0600

    Task-2 basic function parametter problem solved - multiply of 4 numbers

---
+++ problem2.js
@@ -0,0 +1,11 @@
+
+// Task
+// Take four parameters. Multiply the four numbers and then return the result
+
+function fourNumber(a,b,c,d) {
+          const sum = a + b + c + d;
+          const result = sum;
+          return result;
+}
+const finalResult = fourNumber(1500,2500,2500,3500);
+console.log(finalResult);

898c3730071e7448f83ecb1f5a94a6b08e1c214f
Md.Sahriar Alam  Siyam <hackinghobby06@gmail.com>
2026-03-05 03:28:15 +0600

    Remove duplicate items from an array

---
+++ problem5.js
@@ -0,0 +1,16 @@
+const city = ['dhaka', 'tangail','rajsahi','dinajpur','tangail', 'dhaka','dinajpur'];
+
+function noDuplicate(arr) {
+    const uniqueArr = [];
+    for(const item of arr) {
+
+      if(uniqueArr.includes(item) === false ) {
+           uniqueArr.push(item);
+      }
+    }
+    return uniqueArr;
+
+}
+
+const unique = noDuplicate(city);
+console.log(unique);

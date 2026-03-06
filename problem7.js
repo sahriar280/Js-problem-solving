170bc884193f17045d9bc0c7b350a05490a85e4f
Md.Sahriar Alam  Siyam <hackinghobby06@gmail.com>
2026-03-06 23:27:33 +0600

    problem-5 solved

--- problem7.js
+++ problem7.js
@@ -1,24 +1,15 @@
-
 const jim = 200;
 const tim = 100;
 const kim = 90;
-        
 
-function ultimateBoss(num1, num2, num3 ) {
-       if(jim > tim && jim > kim) {
-        console.log('Jim is the boss ');
-       }
-       else if (tim > kim && tim > jim) {
-        console.log('Tim is the mastermind');
-       }
-       else {
-        console.log("kim is the Boss !!!!");
-       }
-
-
-
+function ultimateBoss(num1, num2, num3) {
+  if (jim > tim && jim > kim) {
+    console.log("Jim is the boss ");
+  } else if (tim > kim && tim > jim) {
+    console.log("Tim is the mastermind");
+  } else {
+    console.log("kim is the Boss !!!!");
+  }
 }
 
-
 const Boss = ultimateBoss(jim, tim, kim);
-
